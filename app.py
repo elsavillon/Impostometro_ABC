41fb8ea95b2385a29ef7bcacdfc90aedae9a2bc0
Elsa Villon <elsa.villon@gmail.com>
2024-04-12 02:31:53 -0300

    Ajustse 12040231

--- app.py
+++ app.py
@@ -103,25 +103,29 @@ impostos_municipais = {
 def home():
     return render_template('index.html', abc=abc, sa=sa, sbc=sbc, scs=scs, diadema=diadema, maua=maua, rp=rp, rgs=rgs)
 
-@app.route('/busca', methods=['POST'])
+@app.route('/busca', methods=['GET', 'POST'])
 def busca():
-    municipio = request.form['nome']
-    if municipio == "Santo André":
-        return render_template('sa.html', sa_impostos_municipais=impostos_municipais['sa'])
-    elif municipio == "São Bernardo do Campo":
-        return render_template('sbc.html', sbc_impostos_municipais=impostos_municipais['sbc'])
-    elif municipio == "São Caetano do Sul":
-        return render_template('scs.html', scs_impostos_municipais=impostos_municipais['scs'])
-    elif municipio == 'Diadema':
-        return render_template('diadema.html', diadema_impostos_municipais=impostos_municipais['diadema'])
-    elif municipio == 'Mauá':
-        return render_template('maua.html', maua_impostos_municipais=impostos_municipais['maua'])
-    elif municipio == "Ribeirão Pires":
-        return render_template('rp.html', rp_impostos_municipais=impostos_municipais['rp'])
-    elif municipio == "Rio Grande da Serra":
-        return render_template('rgs.html', rgs_impostos_municipais=impostos_municipais['rgs'])
+    if request.method == 'POST':
+        municipio = request.form['nome']
+        if municipio == "Santo André":
+            return render_template('sa.html', sa_impostos_municipais=impostos_municipais['sa'])
+        elif municipio == "São Bernardo do Campo":
+            return render_template('sbc.html', sbc_impostos_municipais=impostos_municipais['sbc'])
+        elif municipio == "São Caetano do Sul":
+            return render_template('scs.html', scs_impostos_municipais=impostos_municipais['scs'])
+        elif municipio == 'Diadema':
+            return render_template('diadema.html', diadema_impostos_municipais=impostos_municipais['diadema'])
+        elif municipio == 'Mauá':
+            return render_template('maua.html', maua_impostos_municipais=impostos_municipais['maua'])
+        elif municipio == "Ribeirão Pires":
+            return render_template('rp.html', rp_impostos_municipais=impostos_municipais['rp'])
+        elif municipio == "Rio Grande da Serra":
+            return render_template('rgs.html', rgs_impostos_municipais=impostos_municipais['rgs'])
+        else:
+            return 'Município não encontrado.'
     else:
-        return 'Município não encontrado.'
+        # Se a solicitação for um GET, redirecione para a página inicial
+        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
